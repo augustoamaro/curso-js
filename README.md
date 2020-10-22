60b767c09256da003fb7c2c4f02222e2fc8caa65
Augusto Amaro <augustoamaro@icloud.com>
2020-10-22 10:29:01 -0300

    inclusão dos fundamentos array,bool,obj, template

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # curso-js
+
+https://www.udemy.com/course/curso-web/
---
+++ exerciciosJS/fundamentos/objeto.js
@@ -0,0 +1,13 @@
+const produto1 = {}
+produto1.nome = 'Celular'
+produto1.preco = 5000.90
+produto1['desconto'] = 0.40
+
+console.log(produto1)
+
+const produto2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90,    
+}
+
+console.log(produto2);
