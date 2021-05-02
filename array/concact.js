f8b9791415823b76edfac2a09894ea47f8f69ded
Augusto Amaro <augustoamaro@icloud.com>
2021-05-02 15:20:12 -0300

    Studying array examples

---
+++ array/concact.js
@@ -0,0 +1,7 @@
+const filhas = ['Ana', 'Maria']
+const filhos = ['João', 'Pedro']
+
+const todos = filhas.concat(filhos, 'Lebron')
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1, 2], [3, 4], 5, [6, 7]))
