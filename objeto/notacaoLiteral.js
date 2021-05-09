0ef3a195c696ff8c251c6cbb9cc92f1197f19a8f
Augusto Amaro <augustoamaro@icloud.com>
2021-05-09 16:24:21 -0300

    Studying obcject

---
+++ objeto/notacaoLiteral.js
@@ -0,0 +1,29 @@
+// Melhorias referente ao Objeto ES6
+
+const a = 1
+const b = 2
+const c = 3
+
+const obj1 = {a: a, b: b, c: c}
+const obj2 = {a, b, c}
+console.log(obj1, obj2)
+
+const nomeAttr = 'nota'
+const valorAttr = 7.87
+
+const obj3 = {}
+obj3[nomeAttr] = valorAttr
+console.log(obj3)
+
+const obj4 = {[nomeAttr]: valorAttr}
+
+const obj5 = {
+    funcao1: function() {
+
+    }, 
+    funcao2: function() {
+
+    }
+}
+
+console.log(obj5)
---
+++ objeto/objetoConstantes.js
@@ -0,0 +1,22 @@
+// pessoa aponta para esse endereço de memoria -> 123 -> {...}
+const pessoa = { nome: 'Joao'}
+pessoa.nome = 'Pedro'
+console.log(pessoa)
+
+//pessoa -> 456 -> {...} 
+//pessoa = { nome: 'Ana'} erro
+
+Object.freeze(pessoa) // freeza congela o objeto, não deixa mexer no objeto
+
+pessoa.nome = 'Maria'
+pessoa.end = 'Rua ABC'
+delete pessoa.nome
+
+console.log(pessoa.nome)
+console.log(pessoa)
+
+const pessoaConstante = Object.freeze({ nome: 'Joao'})
+pessoaConstante.nome = 'Maria'
+console.log(pessoaConstante)
+
+
